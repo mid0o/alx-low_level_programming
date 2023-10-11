02bb472d86cededeeee8da15114a9d34f2ad4271
Mohamed Saeed <mohamedev@outlook.com>
2023-10-10 19:50:14 -0700

    try fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,10 +18,14 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}
