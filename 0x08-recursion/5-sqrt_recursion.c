24de64cc5b9a1ef3f8e3142cff45cece33681854
mid0o <96616586+mid0o@users.noreply.github.com>
2023-10-26 04:03:57 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,18 +11,19 @@
 */
 int _sqrt_recursion_wrapper(int n, int min, int max)
 {
-	if (min > max)
-	return (-1);
+    int guess;  // Declare guess at the beginning of the function
 
-	int guess = (min + max) / 2;
-	int guess_squared = guess * guess;
+    guess = (min + max) / 2;    /* Get guess (g) */
+    int guess_squared = guess * guess;    /* Square guess (sg) */
 
-	if (guess_squared == n)
-	return (guess);
-	else if (guess_squared < n)
-	return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else
-	return (_sqrt_recursion_wrapper(n, min, guess - 1));
+    if (guess_squared == n)    /* If squared guess == n return guess */
+        return guess;
+    else if (min == max)    /* This means n doesn't have a perfect square */
+        return -1;
+    else if (guess_squared < n)    /* Overshoot g if sg is less than the n */
+        return _sqrt_recursion_wrapper(n, guess + 1, max);
+    else    /* Undershoot g if sg is more than the n */
+        return _sqrt_recursion_wrapper(n, min, guess - 1);
 }
 
 /**
