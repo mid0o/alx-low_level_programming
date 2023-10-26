57199c09685022d01da5e9801287dcdf44c05989
mid0o <96616586+mid0o@users.noreply.github.com>
2023-10-26 04:30:34 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,22 +9,21 @@
  *
  * Return: square root of @n or -1
 */
-int _sqrt_recursion_helper(int n, int min, int max)
+int _sqrt_recursion_wrapper(int n, int min, int max)
 {
-    if (min > max) {
-        return (-1);
-    }
-    
-    int guess = (min + max) / 2;
-    int guess_squared = guess * guess;
-    
-    if (guess_squared == n) {
-        return guess;
-    } else if (guess_squared < n) {
-        return _sqrt_recursion_helper(n, guess + 1, max);
-    } else {
-        return _sqrt_recursion_helper(n, min, guess - 1);
-    }
+	int guess, guess_squared;
+
+	guess = (min + max) / 2;		/*get guess (g)*/
+	guess_squared = guess * guess;		/*square guess (sg)*/
+
+	if (guess_squared == n)			/*if squared guess == n return guess*/
+		return (guess);
+	else if (min == max)			/*this means n doesn't have a perfect square*/
+		return (-1);
+	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
+		return (_sqrt_recursion_wrapper(n, guess + 1, max));
+	else					/*undershoot g if sg is more than the n*/
+		return (_sqrt_recursion_wrapper(n, min, guess - 1));
 }
 
 /**
@@ -37,12 +36,12 @@ int _sqrt_recursion_helper(int n, int min, int max)
 */
 int _sqrt_recursion(int n)
 {
-    if (n < 0) {
-        return (-1);
-    }
-    if (n == 0 || n == 1) {
-        return (n);
-    }
-    
-    return _sqrt_recursion_helper(n, 0, n);
+	if (n == 1)		/*sqrt(1) == 1*/
+		return (1);
+	else if (n == 0)	/*sqrt(0) == 0*/
+		return (0);
+	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+		return (-1);
+	else
+		return (_sqrt_recursion_wrapper(n, 1, n));
 }
