876aba450db493e50438e2d017140b5da265a3e0
mid0o <96616586+mid0o@users.noreply.github.com>
2023-10-28 05:30:26 +0200

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print program name, followed by new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
