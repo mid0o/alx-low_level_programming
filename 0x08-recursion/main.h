10a1374202ee23fc0f512f2a6eecab3867ab1da1
mid0o <96616586+mid0o@users.noreply.github.com>
2023-10-28 04:18:00 +0200

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -9,4 +9,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
