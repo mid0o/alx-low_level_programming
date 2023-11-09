2b33ab018672e42ed932379b865da8e7d94e2b94
mid0o <96616586+mid0o@users.noreply.github.com>
2023-11-10 01:03:13 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+
 /**
- * print_name - print a name
- * Description: this function takes a pointer to a string and a pointer
- * to a function, and then call the function @f paasing the name a sparameter
- *@name: a pointer to a string
- *@f: a pointer to a function
+ * print_name - a function that prints a name.
+ * @name: the name given
+ * @f: function of name
+ *
+ * Rteurn: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-  f(name);
-{
+	if (name != NULL && f != NULL)
+
+		f(name);
+}
